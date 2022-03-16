5f79995afa2b16fefa2372a8c44714e5ef56a2e1
Jay Abella <Jenny.abella1@gmail.com>
2022-03-16 16:15:22 -0700

    Implement feed, pins

--- src/container/Pins.jsx
+++ src/container/Pins.jsx
@@ -7,7 +7,7 @@ const Pins = ( { user } ) => {
   const [searchTerm, setSearchTerm] = useState('');  
   return (
     <div className="px-2 md:px-5">
-      <div classname="bg-gray-50">
+      <div className="bg-gray-50">
         <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} user={user} />
       </div>
       <div className="h-full">
--- src/utils/data.js
+++ src/utils/data.js
@@ -3,3 +3,54 @@ export const userQuery = (userId) => {
     const query = `*[_type == "user" && _id == '${userId}']`
     return query;
 }
+
+// Fetching category data
+export const searchQuery = (searchTerm) => {
+    const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
+          image{
+            asset->{
+              url
+            }
+          },
+              _id,
+              destination,
+              postedBy->{
+                _id,
+                userName,
+                image
+              },
+              save[]{
+                _key,
+                postedBy->{
+                  _id,
+                  userName,
+                  image
+                },
+              },
+            }`;
+    return query;
+  };
+
+// Get all pins, newest posts at top
+export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
+    image{
+      asset->{
+        url
+      }
+    },
+        _id,
+        destination,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+        save[]{
+          _key,
+          postedBy->{
+            _id,
+            userName,
+            image
+          },
+        },
+      } `;

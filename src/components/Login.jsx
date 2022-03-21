c25c0fdf5c0a274cc416fe66e92da063a82c9c67
Jay Abella <Jenny.abella1@gmail.com>
2022-03-20 19:33:57 -0700

    Update Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,7 @@ import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 import shareVideo from '../assets/share.mp4';
-import logowhite from '../assets/logowhite.png';
+import logoWhite from '../assets/logowhite.png'
 
 import { client } from '../client';
 
@@ -11,21 +11,26 @@ const Login = () => {
   const navigate = useNavigate();
   const responseGoogle = (response) => {
     localStorage.setItem('user', JSON.stringify(response.profileObj));
+
     const { name, googleId, imageUrl } = response.profileObj;
+
+    // create new sanity document
     const doc = {
       _id: googleId,
       _type: 'user',
       userName: name,
       image: imageUrl,
-    };
-    client.createIfNotExists(doc).then(() => {
-      navigate('/', { replace: true });
-    });
-  };
+    }
 
+    client.createIfNotExists(doc)
+      .then(() => {
+        navigate('/', { replace: true })
+      })
+
+  }
   return (
     <div className="flex justify-start items-center flex-col h-screen">
-      <div className=" relative w-full h-full">
+      <div className="relative w-full h-full">
         <video
           src={shareVideo}
           type="video/mp4"
@@ -38,7 +43,7 @@ const Login = () => {
 
         <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
           <div className="p-5">
-            <img src={logowhite} width="130px" alt="logo" />
+            <img src={logoWhite} width="130px" alt="logo" />
           </div>
 
           <div className="shadow-2xl">
@@ -62,7 +67,7 @@ const Login = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
